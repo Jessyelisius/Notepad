4b1b1a2b3d54f9eccfe4a682bf39e9a9e063d571
jessyelisius <jecintaelisius2020@gmail.com>
2025-01-10 15:21:28 +0100

    reg

--- app.js
+++ app.js
@@ -2,17 +2,25 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
 
 const app = express();
 const port = process.env.PORT || 3001
 
 ////////////middleware//////////
-app.use(cors())
-app.use(morgan('dev'))
-// app.use('view engine', 'ejs')
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-})
+////view engine//////////////////
+app.set('view engine', 'ejs');
+
+
+/////////////routes/////////////////
+app.use('/auth', require('./routes/Auth.routes'));
 
 app.listen(port, () => console.log(`app listening on port ${port}!`))
--- routes/Auth.routes.js
+++ routes/Auth.routes.js
@@ -1,4 +1,41 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const UserModel = require('../models/UserModel');
 const router = express.Router();
 
-router.post('/', )
+router.get('/', (req, res) => {
+    res.render('register');
+});
+
+router.post('/', async(req, res) => {
+    try {
+        const collect = req.body;
+        if(!collect.FirstName)return res.status(400).render('register',{Msg: "Firstname is required"});
+        if(!collect.LastName)return res.status(400).render('register',{Msg: "Lastname is required"});
+        if(!collect.Email)return res.status(400).render('register',{Msg: "Email is required"});
+        if(!collect.Password.length<4)return res.status(400).render('register',{Msg: "Password is short"});
+        collect.Password = bcrypt.hashSync(collect.Password, 5);
+         
+        await UserModel.create(collect);
+        res.redirect('login');
+    
+    } catch (error) {
+        console.log(error);
+        res.status(400).render('register', {Msg: "Error creating user"});
+    }
+});
+
+router.get('/login', (req, res) => {
+    res.render('login')
+})
+// router.post('/login', async(req, res) => {
+// try {
+//     const Input = req.body;
+//     if(!Input)return res.render('login',{Msg: "login credentials not valid"});
+
+// } catch (error) {
+    
+// }
+// });
+
+module.exports = router;

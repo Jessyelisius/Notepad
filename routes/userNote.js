be30052b128ccd27a5553e81d27dfdd644cde40a
jessyelisius <jecintaelisius2020@gmail.com>
2025-02-02 22:39:32 +0100

    view done

--- routes/userNote.js
+++ routes/userNote.js
@@ -4,9 +4,9 @@ const CreateNoteModel = require('../models/CreateNote.Model');
 
 const router = express.Router();
 
-router.get('/', ValidateToken, async(req, res) => {
-    res.render('index',{ user: req.user, Notes:[], Msg:"" });
-});
+// router.get('/', ValidateToken, async(req, res) => {
+//     res.render('index',{ Notes:[], Msg:"" });
+// });
 
 router.get('/createNote', ValidateToken, async(req, res) => {
     res.render('createNote',{ Msg:""});
@@ -33,25 +33,35 @@ router.post('/create', ValidateToken, async(req, res) => {
     }
 });
 
-router.get('/getAllNotes', ValidateToken, async(req, res) => {
+router.get('/', ValidateToken, async(req, res) => {
     try {
-        console.log('fetching notes for user:', req.user.id);
-        console.log('user id:', req.user.id);
-        
         const Notes = await CreateNoteModel.find({UserId:req.user._id});
-        console.log('notes form user', Notes);
-        
-        if(Notes.length === 0){
-            console.log('no note found for this user');
-            
-            return res.status(400).render('index', {user: req.user, Notes:[], Msg:"No Note available to display"});
-        }
 
-        res.status(200).render('index',{user:req.user, Notes, Msg:""});
-
+        res.render('index', {
+            Notes: Notes.length ? Notes : [],
+            Msg: Notes.length ? null : "No Notes available."
+        });
     } catch (error) {
         console.log("error trying to get Notes", error);
-        res.status(400).render('index',{user:req.user, Notes:[], Msg:"error displaying notes"})
+        res.status(400).render('index',{ Notes:[], Msg:"error displaying notes"});
+    }
+});
+
+router.get('/getSingleNote',ValidateToken, async(req, res) => {
+    res.render('viewNote',{user, Msg:""})
+});
+
+router.get('/getSingleNote/:id', ValidateToken, async(req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await CreateNoteModel.findById(id)
+        if(!user){
+            return res.status(400).render('index',{Msg:"No user with specified id"})
+        }
+        res.status(200).render('viewNote', {user, Msg:""})
+    } catch (error) {
+        console.log("error trying to get a Notes", error);
+        res.status(400).render('index',{ Notes:[], Msg:"error displaying notes"});
     }
 });
 
